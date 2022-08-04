49d16a3fe5dee7aea26c5f14d6dac5d7f35d6bfc
sabuj234 <42721165+sabuj234@users.noreply.github.com>
2022-08-04 20:19:44 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Auth from './pages/Auth/Auth';
-import Home from './pages/Homepage/Home'; 
-import Profile from './pages/Profile/Profile';
-import Login from './pages/Login/Login';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth/Auth";
+import Profile from "./pages/Profile/Profile";
+import { useSelector } from "react-redux";
+import Chat from "./pages/Chat/Chat";
 
 function App() {
+  const user = useSelector((state) => state.authReducer.authData);
   return (
-    <div className="App">
-      <div className='blur' style={{top:'-18%', right:0}}></div>
-      <div className='blur' style={{top:"36%", left :'-8rem'}}></div>
+    <div
+      className="App"
+      style={{
+        height:
+          window.location.href === "http://localhost:3000/chat"
+            ? "calc(100vh - 2rem)"
+            : "auto",
+      }}
+    >
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="profile" element={<Profile/>} />
-        <Route path="signup" element={<Auth/>} />
-        <Route path="login" element={<Login/>} />
+        <Route
+          path="/"
+          element={user ? <Navigate to="home" /> : <Navigate to="auth" />}
+        />
+        <Route
+          path="/home"
+          element={user ? <Home /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="/auth"
+          element={user ? <Navigate to="../home" /> : <Auth />}
+        />
+        <Route
+          path="/profile/:id"
+          element={user ? <Profile /> : <Navigate to="../auth" />}
+        />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+
+        <Route
+          path="/chat"
+          element={user ? <Chat /> : <Navigate to="../auth" />}
+        />
       </Routes>
     </div>
   );
